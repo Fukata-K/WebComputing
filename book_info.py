02ae6a7f345234bdb3f275e799b0b031bcee5bdd
Fukata-K <fukata.keiichi.q1@dc.tohoku.ac.jp>
2024-07-08 20:27:15 +0900

    Update book_info.py

--- book_info.py
+++ book_info.py
@@ -4,37 +4,36 @@ import requests
 
 # book_infoを受け取り表示する関数
 def display_book_info(book_info):
-    
+
     st.write('タイトル: ', book_info['タイトル'])
-    
-    try:
-        st.image(book_info["画像"], '本の画像')
+
+    try: st.image(book_info["画像"], '本の画像')
     except Exception as _: pass
-    
+
     try: st.write('著者　　: ', ' ・ '.join(book_info['著者']))
     except KeyError as _: pass
-        
+
     try: st.write('出版社　: ', book_info['出版社'])
     except KeyError as _: pass
-    
+
     try: st.write('出版日　: ', book_info['出版日'])
     except KeyError as _: pass
-    
+
     try: st.write('ページ数: ', str(book_info['ページ数']))
     except KeyError as _: pass
-    
+
     try: st.write('推薦理由: ', book_info['推薦理由'])
     except KeyError as _: pass
-    
+
     try: st.write('要約　　: ', book_info['要約'])
     except KeyError as _: pass
-    
+
     try: st.write('カテゴリ: ', ' ・ '.join(book_info['カテゴリ']))
     except KeyError as _: pass
-    
+
     try: st.write('Googleブックスのリンク　: ', book_info['本のリンク'])
     except KeyError as _: pass
-    
+
     st.write('\n')
     st.write('\n')
 
